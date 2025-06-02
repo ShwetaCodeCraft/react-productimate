af61345acd3c1f90ddb4c6ce9be42ef3048e4eb7
Shweta Singh <Singh.shweta2973@gmail.com>
2025-06-02 14:09:28 +0530

    assignment-3

---
+++ ant-d/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'antd/dist/reset.css'; // For Ant Design v5
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
